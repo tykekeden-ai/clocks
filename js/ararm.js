859a43f630ddc87709c5fcefb77bbb8de14d6059
ヒナペンギン <t.y.kekeden@gmail.com>
2026-08-30 13:48:00 +0900

    Refactor alarm button event listener setup

--- js/ararm.js
+++ js/ararm.js
@@ -1,10 +1,6 @@
-document.addEventListener("DOMContentLoaded", () => {
+const button = document.getElementById("alarm-settings");
+const panel = document.getElementById("alarm-setting-panel");
 
-    const button = document.getElementById("alarm-settings");
-    const panel = document.getElementById("alarm-setting-panel");
-
-    button.addEventListener("click", () => {
-        panel.hidden = !panel.hidden;
-    });
-
+button.addEventListener("click", function () {
+    panel.hidden = !panel.hidden;
 });
