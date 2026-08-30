7c30995758972e32af5ad5270ba585dda5929a96
ヒナペンギン <t.y.kekeden@gmail.com>
2026-08-30 15:41:42 +0900

    Remove empty line in alarm.js

--- js/ararm.js
+++ js/ararm.js
@@ -1,7 +1,6 @@
 alert("alarm.js 動作しました！");
 const button = document.getElementById("alarm-settings");
 const panel = document.getElementById("alarm-setting-panel");
-
 button.addEventListener("click", function () {
     panel.hidden = !panel.hidden;
 });
