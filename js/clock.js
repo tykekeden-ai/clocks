4d921d7b93bfdb677b5176f52233c549fa9edc00
ヒナペンギン <t.y.kekeden@gmail.com>
2026-08-30 11:57:09 +0900

    Update clock.js

--- js/clock.js
+++ js/clock.js
@@ -1,1 +1,17 @@
+function updateClock() {
+    const now = new Date();
 
+    const hours = String(now.getHours()).padStart(2, "0");
+    const minutes = String(now.getMinutes()).padStart(2, "0");
+    const seconds = String(now.getSeconds()).padStart(2, "0");
+
+    const time = `${hours}:${minutes}:${seconds}`;
+
+    document.getElementById("clock").textContent = time;
+}
+
+// 1秒ごとに更新
+setInterval(updateClock, 1000);
+
+// ページを開いた瞬間にも表示
+updateClock();
